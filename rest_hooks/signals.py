c1c1cb4080deb80c7aad0d67862568edf5dda794
Shen Li <shen@fellow.co>
2023-07-16 16:07:36 -0400

    remove providing_args arg for Django4

--- rest_hooks/signals.py
+++ rest_hooks/signals.py
@@ -1,6 +1,9 @@
 from django.dispatch import Signal
 
 
-hook_event = Signal(providing_args=['action', 'instance'])
-raw_hook_event = Signal(providing_args=['event_name', 'payload', 'user'])
-hook_sent_event = Signal(providing_args=['payload', 'instance', 'hook'])
+# providing_args=['action', 'instance']
+hook_event = Signal()
+# providing_args=['event_name', 'payload', 'user']
+raw_hook_event = Signal()
+# providing_args=['payload', 'instance', 'hook']
+hook_sent_event = Signal()
